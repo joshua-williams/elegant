11422d86cad328a7450e95f3c9aee19fb4f1ebe5
Joshua Williams <com.joshua360@gmail.com>
2025-10-05 09:09:05 -0500

    refactor: MigrationRunner consolidate duplicate code blocks

--- lib/MigrationRunner.ts
+++ lib/MigrationRunner.ts
@@ -12,88 +12,57 @@ type MigrationResult = {
 }
 export default class MigrationRunner {
   private config:ElegantConfig;
-  private results:MigrationResult[] = [];
   private migrationPath:string;
 
   async run(direction:'up'|'down' = 'up'):Promise<void> {
     this.config = await getAppConfig()
     this.migrationPath = appPath(this.config.migrations.directory)
     const migrations = await this.getMigrations()
-    switch(direction) {
-    case 'up':
-      await this.up(migrations)
-      break;
-    case 'down':
-      await this.down(migrations)
-      break;
-    }
-    console.log(JSON.stringify(this.results, null, 2))
+    const results = await this.runMigrations(migrations, direction)
+    console.log(JSON.stringify(results, null, 2))
   }
-
-  async up(migrations: Migration[]):Promise<void> {
+  async runMigrations(migrations:Migration[], direction:'up'|'down'):Promise<MigrationResult[]> {
+    const results:MigrationResult[] = []
     for (const migration of migrations) {
-      const result:MigrationResult = {
-        migration: migration.constructor.name,
-        result: 'success',
-        timestamp: Date.now(),
-        duration: 0,
-      }
-      if (!migration.shouldRun()) {
-        result.result = 'skip'
-        this.results.push(result)
-        continue
-      }
-      await migration.up()
-      const connection = migration.getConnection()
-      const db = await Elegant.connection(connection)
-      try {
-        for (const table of migration.schema.tables) {
-          const sql = table.toSql()
-          await db.query(sql)
-          result.duration = Date.now() - result.timestamp
-          this.results.push(result)
-        }
-      } catch (error) {
-        await db.close()
-        result.result = 'error'
-        result.error = error.message
-        this.results.push(result)
-      }
-      await db.close()
+      const result = await this.runMigration(migration, direction)
+      results.push(result)
     }
+    return results;
   }
 
-  async down (migrations:Migration[]):Promise<void> {
-    for(const migration of migrations) {
-      const result:MigrationResult = {
-        migration: migration.constructor.name,
-        result: 'success',
-        timestamp: Date.now(),
-        duration: 0,
-      }
-      if (!migration.shouldRun()) {
-        result.result = 'skip'
-        this.results.push(result)
-        continue
-      }
-      await migration.down()
-      const connection = migration.getConnection()
-      const db = await Elegant.connection(connection)
-      try {
-        for (const table of migration.schema.tables) {
-          const sql = table.toSql()
-          await db.query(sql)
-          result.duration = Date.now() - result.timestamp
-          this.results.push(result)
-        }
-      } catch (error) {
-        await db.close()
-        result.result = 'error'
-        result.error = error.message
-        this.results.push(result)
-      }
-      await db.close()
+  async runMigration(migration:Migration, direction:'up'|'down'):Promise<MigrationResult> {
+    console.log(`Running ${direction} migration: ${migration.constructor.name}`)
+    const result:MigrationResult = {
+      migration: migration.constructor.name,
+      result: 'success',
+      timestamp: Date.now(),
+      duration: 0,
     }
+    if (!migration.shouldRun()) {
+      result.result = 'skip'
+      return result
+    }
+    try {
+      direction === 'up' ? await migration.up() : await migration.down()
+    } catch (error) {
+      result.result = 'error'
+      result.error = error.message
+    }
+    const connection = migration.getConnection()
+    const db = await Elegant.connection(connection)
+    try {
+      for (const table of migration.schema.tables) {
+        const sql = table.toSql()
+        await db.query(sql)
+        result.duration = Date.now() - result.timestamp
+      }
+    } catch (error) {
+      await db.close()
+      result.result = 'error'
+      result.error = error.message
+    }
+    await db.close()
+    return result
   }
 
   async getMigrations():Promise<Migration[]> {
